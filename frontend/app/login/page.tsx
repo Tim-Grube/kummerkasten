02a093114a368c85c00632cc59c31a66e4a3227b
Tim G. <115377141+Plebysnacc@users.noreply.github.com>
2025-07-24 23:23:04 +0200

    Implement: Frontend Middleware (#117)
    Implements a middleware redirecting users to login site if they're not logged in

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,21 +1,13 @@
 "use client"
 
-import {useUser} from "@/components/providers/user-provider";
 import {Card, CardContent, CardTitle} from "@/components/ui/card";
 import LoginForm from "@/app/login/login-form";
-import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 export default function LoginPage() {
-  const {user} = useUser()
-  const router = useRouter()
   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (user) router.push("/tickets")
-  }, [user, router])
-
-  if(loading || user) return null
+  if(loading) return null
 
   return (
     <div className={'flex justify-center items-center grow'}>
